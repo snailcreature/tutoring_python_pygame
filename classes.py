c8b338ddd0147e9f0a381be6bb5d9f9cc151f7c0
snailcreature <samuel.drage@myport.ac.uk>
2024-03-26 18:33:56 +0000

    Player class

--- classes.py
+++ classes.py
@@ -14,6 +14,24 @@ class Ball:
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, tuple(self.pos), self.r)
 
+
+class Player(Ball):
+    def __init__(self):
+        super().__init__(pos=[100,100], velocity=[50,50], r=25, color=(255, 255, 0))
+
+    def update(self, deltaTime):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.pos[0] += self.velocity[0] * deltaTime
+        if keys[pygame.K_LEFT]:
+            self.pos[0] -= self.velocity[0] * deltaTime
+        
+        if keys[pygame.K_DOWN]:
+            self.pos[1] += self.velocity[1] * deltaTime
+        if keys[pygame.K_UP]:
+            self.pos[1] -= self.velocity[1] * deltaTime
+
+
 if __name__ == "__main__":
     pygame.init()
     S_WIDTH = 600
@@ -26,6 +44,8 @@ if __name__ == "__main__":
     ball = Ball()
     ballB = Ball(pos=[S_WIDTH/2, S_HEIGHT/2], velocity=[20,-20])
 
+    player = Player()
+
     running = True
     while running:
         dt = clock.tick()/1000
@@ -37,10 +57,13 @@ if __name__ == "__main__":
         ball.update(dt)
         ballB.update(dt)
 
+        player.update(dt)
+
         screen.fill((225, 225, 225))
 
         ball.draw(screen)
         ballB.draw(screen)
+        player.draw(screen)
 
         pygame.display.update()
     pygame.quit()
